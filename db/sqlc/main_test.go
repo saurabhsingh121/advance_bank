44b27a348f0ee832bfd52210ddfa19a7dbbf92d9
Saurabh Singh <saurabhsingh121@gmail.com>
2024-03-02 17:17:10 +0530

    routes and test

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -12,10 +12,11 @@ import (
 
 var testQueries *Queries
 var testDB *sql.DB
+
 func TestMain(m *testing.M) {
-	config, err := util.LoadConfig("../..") 
-	if  err != nil {
-		log.Fatal("cannot load config:", err)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Print("cannot load config:", err)
 	}
 	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
@@ -23,6 +24,6 @@ func TestMain(m *testing.M) {
 	}
 
 	testQueries = New(testDB)
-	
+
 	os.Exit(m.Run())
 }
